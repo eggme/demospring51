de27d8ad0f3e2f0b28469eb43e87b68f73c8cdb3
kyyeto9984 <kyyeto9984@naver.com>
2020-08-24 14:41:10 +0900

    빈 scope

--- src/main/java/me/whiteship/demospring51/AppRunner.java
+++ src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -1,13 +1,17 @@
 package me.whiteship.demospring51;
 
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
@@ -15,30 +19,29 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     ApplicationContext cxt;
 
+    @Autowired
+    MessageSource messageSource;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Value("${app.name}")
+    String appName;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
         Environment environment = cxt.getEnvironment();
         System.out.println(Arrays.toString(environment.getActiveProfiles()));
         System.out.println(Arrays.toString(environment.getDefaultProfiles()));
-
-        System.out.println("proto");
-
-        System.out.println(cxt.getBean(Proto.class));
-        System.out.println(cxt.getBean(Proto.class));
-        System.out.println(cxt.getBean(Proto.class));
-
-        System.out.println("single");
-
-        System.out.println(cxt.getBean(Single.class));
-        System.out.println(cxt.getBean(Single.class));
-        System.out.println(cxt.getBean(Single.class));
-
-        System.out.println("proto by single");
-
-        System.out.println(cxt.getBean(Single.class).getProto());
-        System.out.println(cxt.getBean(Single.class).getProto());
-        System.out.println(cxt.getBean(Single.class).getProto());
+        System.out.println(environment.getProperty("app.name"));
+        System.out.println(environment.getProperty("app.about"));
+        System.out.println(appName);
+        while(true) {
+            System.out.println(messageSource.getMessage("greeting", new String[]{"Seungjun"}, Locale.KOREA));
+            System.out.println(messageSource.getMessage("greeting", new String[]{"Seungjun"}, Locale.US));
+            Thread.sleep(1000l);
+        }
     }
 }
+
 
--- src/main/java/me/whiteship/demospring51/Demospring51Application.java
+++ src/main/java/me/whiteship/demospring51/Demospring51Application.java
@@ -8,15 +8,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 import java.util.function.Supplier;
 
 @SpringBootApplication
+@PropertySource("classpath:app.properties")
 public class Demospring51Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demospring51Application.class, args);
     }
 
+    @Bean
+    public MessageSource messageSource(){
+       ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+       messageSource.setBasename("classpath:/messages");
+       messageSource.setDefaultEncoding("UTF-8");
+       messageSource.setCacheSeconds(3);
+       return messageSource;
+
+    }
 }
